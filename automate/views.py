e4b2a8774e9001ba726c4bdfd5b6725bd86e1adb
bhatiaharshit07 <bhatiaharshit07@gmail.com>
2021-04-18 21:03:39 +0530

    added routes to insta and whatsapp

--- automate/views.py
+++ automate/views.py
@@ -1,7 +1,5 @@
-
-# Create your views here.
 from django.shortcuts import render
 from django.http.response import HttpResponse
 # Create your views here.
-def home_instagram(request):
-    return HttpResponse("home_instagram")
+def home(request):
+    return HttpResponse("home")
--- whatsapp_automate/views.py
+++ whatsapp_automate/views.py
@@ -1,3 +1,6 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
-
+from django.http.response import HttpResponse
 # Create your views here.
+def home_whatsapp(request):
+    return HttpResponse("home_whatsapp")
