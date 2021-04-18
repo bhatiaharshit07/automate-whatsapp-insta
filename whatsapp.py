ad4836862965547ffd8d593897288b7b40ac085a
bhatiaharshit07 <bhatiaharshit07@gmail.com>
2021-04-18 17:01:47 +0530

    initial build with django

---
+++ whatsapp.py
@@ -0,0 +1,2 @@
+import pywhatkit
+pywhatkit.sendwhatmsg('+919313152973', 'test message', 16, 44)
---
+++ whatsapp_automate/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WhatsappAutomateConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'whatsapp_automate'
