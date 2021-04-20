658eb8e2f64eac4bec000a7560ceea2df70bec42
bhatiaharshit07 <bhatiaharshit07@gmail.com>
2021-04-20 22:39:50 +0530

    minor change in views.py

--- whatsapp_automate/views.py
+++ whatsapp_automate/views.py
@@ -1,6 +1,12 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.http.response import HttpResponse
+import pywhatkit
+
 # Create your views here.
 def home_whatsapp(request):
+    pywhatkit.sendwhatmsg('+919313152973', 'test message', 13, 16)
     return HttpResponse("home_whatsapp")
+
+def result_whatsapp(request):
+    pywhatkit.sendwhatmsg('+919313152973', 'test message', 16, 44)
+    return HttpResponse("result_whatsapp")
